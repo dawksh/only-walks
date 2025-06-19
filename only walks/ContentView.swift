d34d6e7868246d5ff5f0b35dcceb88f006ac0113
daksh <akshd@outlook.com>
2025-06-19 17:36:36 +0530

    Initial Commit

---
+++ only walks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  only walks
+//
+//  Created by Daksh Kulshrestha on 19/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ only walks/only_walksApp.swift
@@ -0,0 +1,17 @@
+//
+//  only_walksApp.swift
+//  only walks
+//
+//  Created by Daksh Kulshrestha on 19/06/25.
+//
+
+import SwiftUI
+
+@main
+struct only_walksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
